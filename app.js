d49fd89ffd24041e4984a58a74bbc39a6e2023d1
geysa.amorim <geysa.amorim@aluno.edu.pi.gov.br>
2026-06-17 13:07:27 +0000

    calculadora

---
+++ app.js
@@ -0,0 +1,14 @@
+const calculadora = require ('./calculadora.js')
+
+let resultadoSoma=calculadora.soma(1,2);
+console.log (resultadoSoma);
+
+let resultadoSubtrair=calculadora.subtrair(1,2);
+console.log (resultadoSubtrair);
+
+
+let resultadoDividir=calculadora.dividir(1,2);
+console.log (resultadoDividir);
+
+let resultadoMultiplicacao=calculadora.multiplicacao(1,2);
+console.log (resultadoMultiplicacao);
---
+++ objfuncao.js
@@ -0,0 +1,19 @@
+const calculadora={
+    numero1:10,
+    numero2:2,
+    multiplicar(){
+      return this.numero1*this.numero2},
+    dividir(){
+      return this.numero1/this.numero2},
+    somar(){
+      return this.numero1+this.numero2},
+    subtrair(){
+      return this.numero1-this.numero2},
+    }
+    
+    console.log(calculadora.multiplicar())
+    console.log(calculadora.dividir())
+    console.log(calculadora.somar())
+    console.log(calculadora.subtrair())
+
+    
