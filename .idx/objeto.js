2bf34bfe34de7a8cf506efb80bb368244f1ff6fa
geysa.amorim <geysa.amorim@aluno.edu.pi.gov.br>
2026-06-22 15:03:21 +0000

    produto.js

--- .idx/objeto.js
+++
@@ -1,8 +0,0 @@
-const meuObjeto = {
-    nome: "javascript",
-    idade: 2026,
-    ativo: true,
-  };
- console.log (meuObjeto.nome);
- console.log (meuObjeto.idade);
- console.log (meuObjeto.ativo);
---
+++ produto.js
@@ -0,0 +1,9 @@
+const produto = {
+    nome: "Notebook Gamer",
+    preco: 4500.00,
+    emEstoque: true
+};
+console.log(produto.nome);
+console.log(produto.preco);
+console.log(produto.emEstoque);
+
