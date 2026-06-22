8e01eb5a5a40303bdc17cbc89004c625567579e8
geysa.amorim <geysa.amorim@aluno.edu.pi.gov.br>
2026-06-22 13:14:12 +0000

    meuObjeto

---
+++ .idx/objeto.js
@@ -0,0 +1,8 @@
+const meuObjeto = {
+    nome: "javascript",
+    idade: 2026,
+    ativo: true,
+  };
+ console.log (meuObjeto.nome);
+ console.log (meuObjeto.idade);
+ console.log (meuObjeto.ativo);
